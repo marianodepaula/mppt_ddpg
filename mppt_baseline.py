f64a66ceccc7a89d3a87c7d237d924c03f193c44
ignaciocarlucho <ignacio.carlucho@gmail.com>
2019-08-09 15:53:53 -0300

    added parser to mppt baseline

--- mppt_baseline.py
+++ mppt_baseline.py
@@ -4,31 +4,29 @@ import numpy as np
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
-#import argparse
+import argparse
 
-'''
+
 parser = argparse.ArgumentParser('deepid')
-parser.add_argument('--epochs', type=int, default=2000)
-parser.add_argument('--test_epochs', type=int, default=2000)
+parser.add_argument('--total_timesteps', type=int, default=2000)
+parser.add_argument('--test_steps', type=int, default=2000)
 parser.add_argument('--verbose', type=int, default=0)
 args = parser.parse_args()
-'''
 
-epochs = 20
-test_epochs=20
-verbose = 0 
+
+
 
 env = gym.make('mppt-v0')
 env = DummyVecEnv([lambda: env])  # The algorithms require a vectorized environment to run
 
 print('training model')
-model = PPO2(MlpPolicy, env, verbose=verbose)
-model.learn(total_timesteps=epochs)
+model = PPO2(MlpPolicy, env, verbose=args.verbose)
+model.learn(total_timesteps=args.total_timesteps)
 
 print('testing the model')
 obs = env.reset()
 print('state =',obs,obs.shape)
-for i in range(test_epochs):
+for i in range(args.test_steps):
     action, _states = model.predict(obs)
     obs, rewards, dones, info = env.step(action)
     print('state =',obs,'r',rewards,'done', dones, 'info',info)
