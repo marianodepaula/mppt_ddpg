da529cfdb7c192007efee81bc512683145778ab6
marianodepaula <mariano.depaula@fio.unicen.edu.ar>
2019-08-15 11:36:13 -0300

    separé el test del train

--- mppt_baseline_test.py
+++ mppt_baseline_test.py
@@ -135,7 +135,8 @@ if __name__ == '__main__':
 		print('accion shape= ', action.shape, type(action))
 		next_state, rewards, dones, info = env1.step(action) #info = {'Corriente': I_new, 'Temperatura':T, 'Irradiancia':G,'Accion':action}
 		print('state =',next_state,'r',rewards,'done', dones, 'info',info,type(info))
-		grafos.add(next_state[0][0], next_state[0][1], next_state[0][2], info['Corriente'], info['Temperatura'], info['Irradiancia'], info['Accion'])
+		#grafos.add(next_state[0][0], next_state[0][1], next_state[0][2], info['Corriente'], info['Temperatura'], info['Irradiancia'], info['Accion'])
+		grafos.add(next_state[0][0], next_state[0][1], next_state[0][2], 1, 1,1,1)
 		print('vamos bien, por la i=',i)
 		np.save('last_state.npy',obs)
     	# y si quisiera levantar tal variable x, hacemos:
