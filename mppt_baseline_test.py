6c43c1a0f2407697119c9cbc63a1740532e041ad
marianodepaula <mariano.depaula@fio.unicen.edu.ar>
2019-08-15 12:25:27 -0300

    separé el test del train

--- mppt_baseline_test.py
+++ mppt_baseline_test.py
@@ -135,7 +135,7 @@ if __name__ == '__main__':
 		print('accion shape= ', action.shape, type(action))
 		next_state, rewards, dones, info = env1.step(action) #info = {'Corriente': I_new, 'Temperatura':T, 'Irradiancia':G,'Accion':action}
 		#print('state =',next_state,'r',rewards,'done', dones, 'info',info,type(info), 'accion', info[0])
-		print('info_0', info[0])
+		print('info_0', info[0], type(info[0]))
 		#grafos.add(next_state[0][0], next_state[0][1], next_state[0][2], info['Corriente'], info['Temperatura'], info['Irradiancia'], info['Accion'])
 		grafos.add(next_state[0][0], next_state[0][1], next_state[0][2], info[0], info[1], info[2], info[3])
 		print('vamos bien, por la i=',i)
